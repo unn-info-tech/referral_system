57fc75f98da09d51ef36d408dd18f9d74a3de840
Nematulloh <unn.info.tech@gmail.com>
2024-12-06 06:33:43 +0300

    Updated

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,13 @@
-# Use an official Python runtime as a parent image
-FROM python:3.11
+FROM python:3.10-slim
 
-# Set environment variables (adjust as needed)
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
-ENV DJANGO_SETTINGS_MODULE config.settings
+WORKDIR /usr/src/app
 
-# Set the working directory to /app
-WORKDIR /app
+COPY requirements.txt .
 
-# Copy the current directory contents into the container at /app
-COPY . /app
-
-# Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Expose the port your application will run on
+COPY . .
+
 EXPOSE 8000
 
-# Run the application
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- user_verification/views.py
+++ user_verification/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from .utils import generate_verification_code
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from twilio.rest import Client
+#from twilio.rest import Client
 from .serializers import PhoneNumberVerificationSerializer
 from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect
@@ -38,11 +38,10 @@ class PhoneNumberVerificationView(APIView):
             )'''
             
 
-            # Store the verification code in the session for later validation
             request.session['verification_code'] = verification_code
             request.session['phone_number'] = phone_number
 
-            time.sleep(2)  # Introduce a 2-second delay
+            time.sleep(2)  
 
             return redirect('verify-code')
         else:
@@ -52,32 +51,26 @@ class PhoneNumberVerificationView(APIView):
 
 class CodeVerificationView(APIView):
     def get(self, request):
-        # Display the verification code entry form
-        return render(request, 'user_verification/verify_code.html')
+        return render(request, 'user_verification/verify_code.html', {"OTP": request.session.get('verification_code')})
 
     def post(self, request):
-        entered_code = request.data.get('user_code')  # Assuming you're using POST data
+        entered_code = request.data.get('user_code')  
         stored_code = request.session.get('verification_code')
 
         if entered_code == stored_code:
-            # Code is correct, perform desired actions
-            # ...
             print("Phone number from session:", request.session.get('phone_number'))
 
             phone_number = request.session.get('phone_number')
             try:
                 user = CustomUser.objects.get(phone_number=phone_number)
             except CustomUser.DoesNotExist:
-                # If user is not registered, create a new user
                 user = CustomUser(phone_number=phone_number)
                 user.save()
 
-                # Generate a random 6-digit invite code
                 invite_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
                 invite = InviteCode(invite_code=invite_code)
                 invite.save()
  
-                # Now set the invite code to the user's my_invite_code field
                 user.my_invite_code = invite
                 user.is_verified = True
                 user.save()
@@ -85,9 +78,7 @@ class CodeVerificationView(APIView):
 
             del request.session['verification_code']
             return redirect('my-page')
-            #return render(request, 'user_verification/verify_success.html')
         else:
-            # Code is incorrect, display error message
             return render(request, 'user_verification/verify_code.html', {'error_message': 'Invalid code'})
         
 
@@ -102,7 +93,6 @@ class MyPageView(APIView):
             return render(request, 'user_verification/error.html', {'error': 'Пользователь не найден'})
         
         if user.is_verified:
-            # Get users with the same friend_invite_code
             friends = CustomUser.objects.filter(friend_invite_code=user.my_invite_code)
             
             context = {
@@ -128,19 +118,15 @@ class VerifyFriendInviteView(APIView):
         if user.is_verified:
             friend_invite_code = request.POST.get('friend_invite_code')
             
-            # Check if the provided friend invite code exists
             try:
                 invite = InviteCode.objects.get(invite_code=friend_invite_code)
             except InviteCode.DoesNotExist:
                 return render(request, 'user_verification/error.html', {'error': 'Неверный код приглашения друга'})
             
-            # Update the user's friend_invite_code
             user.friend_invite_code = invite
             user.save()
             
-            # Redirect to a success page or another view
-            return redirect('my-page')  # You can replace 'my-page' with the appropriate URL name
-        
+            return redirect('my-page') 
         else:
             return render(request, 'user_verification/error.html', {'error': 'Пользователь не проверен'})
 
